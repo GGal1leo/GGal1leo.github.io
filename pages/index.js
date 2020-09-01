9b48550304068635d1d2df66a494ad62163a6117
root <root@localhost.localdomain>
2020-09-01 17:19:40 +0100

    uploading CMS +++

--- pages/index.js
+++ pages/index.js
@@ -10,20 +10,12 @@ const Index = ({ posts, title, description, ...props }) => {
         <h1 className="title">Welcome to this demo blog!</h1>
 
         <p className="description">
-          This is a simple blog built with Next, easily deployable on{' '}
-          <a href="https://url.netlify.com/r1j6ybSYU">Netlify</a>.
+          This is an awesome built with Next
         </p>
         <main>
           <PostList posts={posts} />
         </main>
-        <p>
-          You can look at the repository for this project{' '}
-          <a href="https://github.com/cassidoo/next-netlify-blog-starter">
-            here
-          </a>
-          , and a tutorial on how to build it {` `}
-          <a href="https://url.netlify.com/ByVW0bCF8">here</a>.
-        </p>
+        
       </Layout>
       <style jsx>{`
         .title {
